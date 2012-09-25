88a82deed2c0cc6ceed7cfb1be89e3f19e090130
Mats SX <Mats.SXz@gmail.com>
2012-09-25 15:48:12 +0200

    Implemented most of Graph. Added a pattern to Parser. Implemented a parsing method following this pattern.

--- java_workspace/the_git_project/src/having/fun/with/code/Graph.java
+++ java_workspace/the_git_project/src/having/fun/with/code/Graph.java
@@ -2,6 +2,11 @@ package having.fun.with.code;
 
 import java.util.ArrayList;
 
+/**
+ * A directed multigraph implementation. Support for weighted edges to come.
+ * @author dt08mr7
+ *
+ */
 public class Graph {
 	
 	private ArrayList<Node> nodes;
@@ -12,15 +17,158 @@ public class Graph {
 		edges = new ArrayList<Edge>();
 	}
 	
-	
-	private class Node {
-		private ArrayList<Edge> edgesOut;
-		private ArrayList<Edge> edgesIn;
+	/**
+	 * Fetches and returns the node with the given id, if it exists.
+	 * If it does not exist, a new node with the given id will be created and returned.
+	 * @param id
+	 * @return
+	 */
+	public Node getNode(int id) {
+		for (Node n : nodes) {
+			if (n.id == id) {
+				return n;
+			}
+		}
+		Node n = new Node(id);
+		nodes.add(n);		
+		return n;
 	}
 	
-	private class Edge {
+	/**
+	 * Adds a new edge between nodes from and to.
+	 * @param from
+	 * @param to
+	 */
+	public void addEdge(Node from, Node to) {
+		Edge e = new Edge(from, to);
+		from.addOutEdge(e);
+		to.addInEdge(e);
+		edges.add(e);
+	}
+	
+	/**
+	 * Removes an edge between from and to, if at least one existed.
+	 * @param from
+	 * @param to
+	 */
+	public void removeEdge(Node from, Node to) {
+		for (Edge e : from.edgesOut) {
+			if (e.end.equals(to)) {
+				edges.remove(e);
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * Removes a node from this graph.
+	 * All edges going in or out of this node are also removed.
+	 * @param n
+	 */
+	public void removeNode(Node n) {
+		nodes.remove(n);
+		for (Edge e : edges) {
+			if (e.start.equals(n)) {
+				edges.remove(e);
+				e.end.edgesIn.remove(e);
+			} else if (e.end.equals(n)) {
+				edges.remove(e);
+				e.start.edgesOut.remove(e);
+			}
+		}
+	}
+	
+	private static class Node {
+		private ArrayList<Edge> edgesOut;
+		private ArrayList<Edge> edgesIn;		// for directed graphs
+		private int id;
+		
+		public Node(int id) {
+			edgesOut = new ArrayList<Edge>();
+			edgesIn = new ArrayList<Edge>();
+			this.id = id;
+		}
+		
+		public void addOutEdge(Edge e) {
+			edgesOut.add(e);
+		}
+		
+		public void addInEdge(Edge e) {
+			edgesIn.add(e);
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + id;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Node other = (Node) obj;
+			if (id != other.id)
+				return false;
+			return true;
+		}
+	}
+	
+	private static class Edge {
 		private Node start;
 		private Node end;
+		private double weight;	
+		
+		public Edge(Node start, Node end) {
+			this.start = start;
+			this.end = end;
+			weight = 1;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((end == null) ? 0 : end.hashCode());
+			result = prime * result + ((start == null) ? 0 : start.hashCode());
+			long temp;
+			temp = Double.doubleToLongBits(weight);
+			result = prime * result + (int) (temp ^ (temp >>> 32));
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Edge other = (Edge) obj;
+			if (end == null) {
+				if (other.end != null)
+					return false;
+			} else if (!end.equals(other.end))
+				return false;
+			if (start == null) {
+				if (other.start != null)
+					return false;
+			} else if (!start.equals(other.start))
+				return false;
+			if (Double.doubleToLongBits(weight) != Double
+					.doubleToLongBits(other.weight))
+				return false;
+			return true;
+		}
+		
+		
 	}
 
 }
--- java_workspace/the_git_project/src/having/fun/with/code/Parser.java
+++ java_workspace/the_git_project/src/having/fun/with/code/Parser.java
@@ -1,5 +1,34 @@
 package having.fun.with.code;
 
+
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Parser {
+	private Scanner scan;
+	public static final Pattern EDGE_PATTERN = Pattern.compile("(\\d)\\s(\\d)");
+	public static final Pattern NODE_PATTERN = Pattern.compile("");
+	public static final Pattern EXTRA_PATTERN = Pattern.compile("");
+	
+	public Parser(InputStream is) {
+		scan = new Scanner(is);
+	}
+	
+	public Graph parse(Pattern pattern) {
+		Graph g = new Graph();
+		
+		while (scan.hasNext()) {
+			Matcher m = pattern.matcher(scan.nextLine());
+			if (m.find()) {
+				g.addEdge(g.getNode(Integer.parseInt(m.group(1))), g.getNode(Integer.parseInt(m.group(2))));
+			}
+		}
+		
+		return g;
+	}
+	
+	
 
 }
